e77f5ce18ee69b5d418453bc06c5ffa9de93a68a
Anton <62949848+icepaq@users.noreply.github.com>
2022-02-22 19:30:11 -0500

    Init Verification Code Input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function Verify() {
 
-export default App;
+    const numberChange = (e) => {
+        let id = document.activeElement.id;
+
+        if(e.target.value === 'CALLED_FROM_EVENT_LISTENER') { 
+            // Select the previous input
+            if(document.getElementById(Number.parseInt(id) - 1) != null ) {
+                document.getElementById(Number.parseInt(id) - 1).focus();
+            }
+
+            return;
+        } else {
+            // If the user chooses to paste their code instead of typing it
+            if(e.target.value.length === 6) {
+                let code = e.target.value.split('');
+    
+                for(let i = 0; i < code.length; i++) {
+                    document.getElementById(i + 1).value = code[i];
+                }
+
+                document.getElementById(6).focus();
+
+                return;
+            }
+        }
+
+        // Only allow 1 character to be entered
+        if(e.target.value.length > 1) {
+            e.target.value = e.target.value.slice(0, 1);
+        }
+
+        // Select the next input only if the user has entered a character
+        if(e.target.value !== '') {
+            if(document.getElementById(Number.parseInt(id) + 1) != null ) {
+                document.getElementById(Number.parseInt(id) + 1).focus();
+            }
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keyup', (e) => {
+            // numberChange only accepts an event object
+            const dummyStruct = {
+                target: {
+                    value: 'CALLED_FROM_EVENT_LISTENER'
+                }
+            }
+
+            if(e.code === 'Backspace') {
+                numberChange(dummyStruct);
+            }
+        });
+    }, []);
+
+
+    return (
+        <>
+            <div className="wrapper">
+                <div className="title">
+                    Verify Your Email
+                </div>
+                <div className="subtitle">
+                    Please enter the verification code sent to your email.
+                </div>
+
+                <div className="conainer">
+                    <input id="1" type="text" className="code" onChange={numberChange} />
+                    <input id="2" type="text" className="code" onChange={numberChange} />
+                    <input id="3" type="text" className="code" onChange={numberChange} />
+                    <input id="4" type="text" className="code" onChange={numberChange} />
+                    <input id="5" type="text" className="code" onChange={numberChange} />
+                    <input id="6" type="text" className="code" onChange={numberChange} />
+                </div>
+
+                <div className="button">
+                    Verify
+                </div>
+            </div>
+        </>
+    )
+}
